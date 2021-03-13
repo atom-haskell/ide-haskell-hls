358130bfedf6ff686a2e45ba56ad83d1764682b6
Nikolay Yakimov <root@livid.pp.ru>
2021-03-13 18:32:49 +0300

    Filter empty datatips

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { AutoLanguageClient } from 'atom-languageclient'
+import { TextEditor, Point } from 'atom'
 
 class HLSLanguageClient extends AutoLanguageClient {
   getGrammarScopes() {
@@ -15,6 +16,22 @@ class HLSLanguageClient extends AutoLanguageClient {
     return super.spawn('haskell-language-server-wrapper', ['--lsp'], {
       cwd: projectPath,
     })
+  }
+  //////////////////////////// overrides ///////////////////////////////
+  async getDatatip(editor: TextEditor, point: Point) {
+    // HLS likes to return empty datatips; this tries to filter those out
+    const datatip = await super.getDatatip(editor, point)
+    if (datatip) {
+      if ('component' in datatip) {
+        return datatip
+      } else {
+        datatip.markedStrings = datatip.markedStrings.filter((x) => x.value)
+        if (datatip.markedStrings.length > 0) {
+          return datatip
+        }
+      }
+    }
+    return null
   }
 }
 
