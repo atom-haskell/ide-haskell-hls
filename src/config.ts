01da19312ff93dbfe98fceb8a5e0212c479703c7
Nikolay Yakimov <root@livid.pp.ru>
2021-03-14 17:55:40 +0300

    Simplify config definition a little

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,6 @@ export type TypedConfig<
   U extends Type | never = Type
 > = TypedProps<T, U> & {
   type: T
-  items?: {
-    type: T extends 'array' ? U : never
-  }
 }
 interface BasicProps<T extends Type> {
   title: string
@@ -68,80 +65,85 @@ type TypedProps<T extends Type, U extends ItemType> = BasicProps<T> &
 export interface IConfig {
   [key: string]: TypedConfig
 }
+export type Obj = TypedConfig<'object'>
+export type Str = TypedConfig<'string'>
+export type Bool = TypedConfig<'boolean'>
+export type Int = TypedConfig<'integer'>
+export type Arr<U extends Type> = TypedConfig<'array', U>
 
 export const config: IConfig = {
-  haskell: {
+  haskell: <Obj>{
     type: 'object',
     title: 'Haskell Language Server Settings',
     order: 0,
     properties: {
       // Formatting provider (haskell.formattingProvider, default ormolu): what formatter to use; one of floskell, ormolu, fourmolu, stylish-haskell, or brittany (if compiled with AGPL)
-      formattingProvider: {
+      formattingProvider: <Str>{
         title: 'Formatting provider',
         description: 'what formatter to use',
         type: 'string',
         order: 10,
         enum: ['floskell', 'ormolu', 'fourmolu', 'stylish-haskell', 'brittany'],
         default: 'ormolu',
-      } as TypedConfig<'string'>,
+      },
       // Format on imports (haskell.formatOnImportOn, default true): whether to format after adding an import
-      formatOnImportOn: {
+      formatOnImportOn: <Bool>{
         title: 'Format on imports',
         description: 'whether to format after adding an import',
         type: 'boolean',
         order: 20,
         default: true,
-      } as TypedConfig<'boolean'>,
+      },
       // Maximum number of problems to report (haskell.maxNumberOfProblems, default 100): the maximum number of problems the server will send to the client
-      maxNumberOfProblems: {
+      maxNumberOfProblems: <Int>{
         title: 'Maximum number of problems to report',
         description:
           'the maximum number of problems the server will send to the client',
         type: 'integer',
         order: 30,
         default: 100,
-      } as TypedConfig<'integer'>,
+      },
       // Diagnostics on change (haskell.diagnosticsOnChange, default true): (currently unused)
-      diagnosticsOnChange: {
+      diagnosticsOnChange: <Bool>{
         title: 'Diagnostics on change',
         description: '(currently unused)',
         type: 'boolean',
         order: 40,
         default: true,
-      } as TypedConfig<'boolean'>,
+      },
       // Completion snippets (haskell.completionSnippetsOn, default true): whether to support completion snippets
-      completionSnippetsOn: {
+      completionSnippetsOn: <Bool>{
         title: 'Completion snippets',
         description: 'whether to support completion snippets',
         type: 'boolean',
         order: 50,
         default: true,
-      } as TypedConfig<'boolean'>,
+      },
       // Liquid Haskell (haskell.liquidOn, default false): whether to enable Liquid Haskell support (currently unused until the Liquid Haskell support is functional again)
-      liquidOn: {
+      liquidOn: <Bool>{
         title: 'Liquid Haskell',
         description:
           'whether to enable Liquid Haskell support (currently unused until the Liquid Haskell support is functional again)',
         type: 'boolean',
         order: 60,
         default: false,
-      } as TypedConfig<'boolean'>,
+      },
       // Hlint (haskell.hlintOn, default true): whether to enable Hlint support
-      hlintOn: {
+      hlintOn: <Bool>{
         title: 'Hlint',
         description: 'whether to enable Hlint support',
         type: 'boolean',
         order: 70,
         default: true,
-      } as TypedConfig<'boolean'>,
+      },
       // Max completions (haskell.maxCompletions, default 40): maximum number of completions sent to the LSP client.
-      maxCompletions: {
+      maxCompletions: <Int>{
         title: 'Hlint',
         description: 'maximum number of completions sent to the LSP client',
         type: 'integer',
         order: 80,
         default: 40,
-      } as TypedConfig<'integer'>,
+      },
     },
   },
 }
